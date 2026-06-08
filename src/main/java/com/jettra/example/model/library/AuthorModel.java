2ac41c7bea1906d657d91b8629d2a3d9f5845596
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-06-08 13:36:07 -0500

    RestClientBuilder

--- src/main/java/com/jettra/example/model/library/AuthorModel.java
+++ src/main/java/com/jettra/example/model/library/AuthorModel.java
@@ -6,23 +6,42 @@ import io.jettra.wui.core.annotations.PropertiesInRecord;
 import io.jettra.wui.core.annotations.JettraViewModel;
 
 @JettraViewModel
- @ModelToRecordConversor(goal = Author.class)
+@ModelToRecordConversor(goal = Author.class)
 public class AuthorModel {
+
     @PropertiesInRecord
     private String id;
-    
+
     @PropertiesInRecord
     private String name;
-    
+
     @PropertiesInRecord
     private String country;
 
-    public AuthorModel() {}
+    public AuthorModel() {
+    }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
