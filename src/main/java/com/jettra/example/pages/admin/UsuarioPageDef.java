1bf28d9f6e970ffbd14a6e2d9b20bfc641bcb5ba
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-06-16 09:10:48 -0500

    cancion

--- src/main/java/com/jettra/example/pages/admin/UsuarioPageDef.java
+++ src/main/java/com/jettra/example/pages/admin/UsuarioPageDef.java
@@ -2,11 +2,8 @@ package com.jettra.example.pages.admin;
 
 import com.jettra.example.dashboard.DashboardBasePage;
 import io.jettra.wui.core.annotations.CrudView;
-import io.jettra.wui.core.annotations.InjectProperties;
 import io.jettra.wui.sync.JettraPageSincronized;
 import io.jettra.wui.sync.SyncType;
-import java.util.Properties;
-import io.jettra.wui.complex.Center;
 
 @JettraPageSincronized(SyncType.ALL)
 @CrudView(extendsClass = DashboardBasePage.class, model = com.jettra.example.model.admin.UsuarioModel.class, repository = com.jettra.example.repository.admin.UsuarioRepository.class,
