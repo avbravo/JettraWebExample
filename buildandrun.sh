bf4b2485e749aac67d31fbcb70255e829a0b8722
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-06-22 15:22:44 -0500

    Organizar paquetes y directorios

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,3 +1,6 @@
+echo "Compilando aplicacion y generando .jar"
 mvn clean verify install
+echo "Ejecutando la aplicación"
 
-java -jar target/JettraWebExample-1.0-SNAPSHOT.jar
+java -jar  -XX:+UseCompactObjectHeaders target/JettraWebExample-1.0-SNAPSHOT.jar
+# java -jar  -XX:+UseCompactObjectHeaders target/JettraWebExample-1.0-SNAPSHOT.jar
